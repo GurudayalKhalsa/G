c6698e3919ec66cbef6f4ad64f57b53b8de01f71
GurudayalKhalsa <gurudayalkhalsa@gmail.com>
2014-01-10 13:53:30 -0500

    cleaned up

--- gruntfile.js
+++ gruntfile.js
@@ -10,7 +10,6 @@ module.exports = function(grunt){
     require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
 
     grunt.initConfig({
-
         pkg: grunt.file.readJSON('package.json'),
 
         src: [
